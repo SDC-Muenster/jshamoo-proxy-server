eda4d599bbfc89230512e81ee13335fddf00f112
Jenny Shamoo <jenny.shamoo@gmail.com>
2019-11-14 22:14:04 -0800

    update the localhost url to live url

---
+++ proxy-server/k6/http_get.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '60s', target: 1 },
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 100 },
+    { duration: '60s', target: 1000 },
+    { duration: '60s', target: 100 },
+  ],
+  rps: 1000,
+};
+
+export default function(){
+  let id = Math.floor(Math.random() * 10000000 + 1);
+  let res = http.get(`http://localhost:4000?id=${id}`);
+  check(res, {
+    'status was 200': (r) => r.status == 200,
+    'transaction time OK': (r) => r.timings.duration < 200,
+  });
+}
--- proxy-server/server/server.js
+++ proxy-server/server/server.js
@@ -1,3 +1,5 @@
+require('newrelic');
+require('dotenv').config();
 let express = require('express');
 let app = express();
 let proxy = require('http-proxy-middleware');
@@ -6,7 +8,7 @@ let proxy = require('http-proxy-middleware');
 app.use(proxy('/api/photos', {target: 'http://localhost:3333'}));
 app.use(proxy('/house', {target: 'http://localhost:3005'}));
 app.use(proxy('/api/houses', {target: 'http://localhost:3000'}));
-app.use(proxy('/homes', {target: 'http://localhost:1022'}));
+app.use(proxy('/homes', { target: 'http://3.16.255.209:3022'}));
 app.use(express.static('public'));
 
 
